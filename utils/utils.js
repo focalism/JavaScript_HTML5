1e97d7c6f228326629395c0ac5999863ae574f3e
focalism <xzg.nju@gmail.com>
2017-06-27 07:50:37 +0800

    chapter 8 and chapter 9

--- utils/utils.js
+++ utils/utils.js
@@ -92,3 +92,11 @@ utils.colorToRGB = function(color, alpha) {
         return "rgba(" + r + "," + g + "," + b + "," + a + ")";
     }
 };
+
+
+utils.intersects = function (rectA, rectB) {
+  return !(rectA.x + rectA.width < rectB.x ||
+           rectB.x + rectB.width < rectA.x ||
+           rectA.y + rectA.height < rectB.y ||
+           rectB.y + rectB.height < rectA.y);
+};
