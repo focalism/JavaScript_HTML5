908f15e17deb7c8ae9a6eec6ace2c98a1da45bbe
focalism <xzg.nju@gmail.com>
2017-08-03 00:10:55 +0800

    14章作业

--- utils/utils.js
+++ utils/utils.js
@@ -100,3 +100,11 @@ utils.intersects = function (rectA, rectB) {
            rectA.y + rectA.height < rectB.y ||
            rectB.y + rectB.height < rectA.y);
 };
+
+
+utils.containsPoint = function (rect, x, y) {
+  return !(x < rect.x ||
+           x > rect.x + rect.width ||
+           y < rect.y ||
+           y > rect.y + rect.height);
+};
